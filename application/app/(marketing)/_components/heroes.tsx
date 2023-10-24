a43f5c355992b512846ae3126e1994b303fbd12a
palazkouruslan <r.palazkou@melsoft-games.com>
2023-10-24 15:01:06 +0300

    second commit

--- application/app/(marketing)/_components/heroes.tsx
+++ application/app/(marketing)/_components/heroes.tsx
@@ -6,16 +6,28 @@ const Heroes = () => {
       <div className="flex items-center">
         <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
           <Image
-            className="object-contain"
+            className="object-contain dark:hidden"
             src="/documents.png"
+            fill
+            alt="Documents"
+          />
+          <Image
+            className="object-contain hidden dark:block"
+            src="/documents-dark.png"
             fill
             alt="Documents"
           />
         </div>
         <div className="relative h-[400px] w-[400px] hidden md:block">
           <Image
-            className="object-contain"
+            className="object-contain dark:hidden"
             src="/reading.png"
+            fill
+            alt="Reading"
+          />
+          <Image
+            className="object-contain hidden dark:block"
+            src="/reading-dark.png"
             fill
             alt="Reading"
           />
--- application/app/layout.tsx
+++ application/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,7 +31,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="potion-theme-2"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
