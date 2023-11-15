0b75ac669bd21fa7a2268ccd265f9275c80524de
palazkouruslan <r.palazkou@melsoft-games.com>
2023-11-15 10:09:54 +0300

    reorginize

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -3,10 +3,11 @@
 import Banner from "@/app/(main)/_components/banner";
 import Menu from "@/app/(main)/_components/menu";
 import Publish from "@/app/(main)/_components/publish";
-import { api } from "@/convex/_generated/api";
-import { useQuery } from "convex/react";
-import { MenuIcon } from "lucide-react";
-import { useParams } from "next/navigation";
+import {api} from "@/convex/_generated/api";
+import {Id} from "@/convex/_generated/dataModel";
+import {useQuery} from "convex/react";
+import {MenuIcon} from "lucide-react";
+import {useParams} from "next/navigation";
 import React from "react";
 import Title from "./title";
 
@@ -15,17 +16,17 @@ interface NavbarProps {
   onResetWidth: () => void;
 }
 
-const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
+const Navbar = ({isCollapsed, onResetWidth}: NavbarProps) => {
   const params = useParams();
   const document = useQuery(api.documents.getById, {
-    documentId: params.documentId,
+    documentId: params.documentId as Id<"documents">,
   });
   if (document === undefined) {
     return (
       <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
-        <Title.Skeleton />
+        <Title.Skeleton/>
         <div className="flex items-center gap-x-2">
-          <Menu.Skeleton />
+          <Menu.Skeleton/>
         </div>
       </nav>
     );
@@ -46,14 +47,14 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
           />
         )}
         <div className="flex items-center justify-between w-full">
-          <Title initialData={document} />
+          <Title initialData={document}/>
           <div className="flex items-center gap-x-2">
-            <Publish initialData={document} />
-            <Menu documentId={document._id} />
+            <Publish initialData={document}/>
+            <Menu documentId={document._id}/>
           </div>
         </div>
       </nav>
-      {document.isArchived && <Banner documentId={document._id} />}
+      {document.isArchived && <Banner documentId={document._id}/>}
     </>
   );
 };
--- components/providers/convex-provider.tsx
+++ components/providers/convex-provider.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { ReactNode } from "react";
-import { ConvexReactClient } from "convex/react";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import {ClerkProvider, useAuth} from "@clerk/clerk-react";
+import {ConvexReactClient} from "convex/react";
+import {ConvexProviderWithClerk} from "convex/react-clerk";
+import {ReactNode} from "react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
-export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
+export const ConvexClientProvider = ({children}: { children: ReactNode }) => {
   return (
+    // @ts-ignore
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
